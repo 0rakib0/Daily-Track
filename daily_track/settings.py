ee92c12a8f0714058d504a26082687c01d13994c
Rakibul Hasan <hassanrakibul926@gmail.com>
2025-02-06 01:25:16 +0600

    create budget category models and budget models

--- daily_track/settings.py
+++ daily_track/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'accounts',
     'home',
     'transactions',
+    'utils',
 ]
 
 MIDDLEWARE = [
---
+++ utils/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+
+class BudgetCategory(models.Model):
+    cat_name = models.CharField(max_length=160)
+    create_at = models.DateTimeField(auto_now_add=True) 
+
+
+    def __str__(self):
+        return self.cat_name
+
+class Budget(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    category = models.ForeignKey(BudgetCategory, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    due_date = models.DateField()
+    is_completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    
+    def __str__(self):
+        return self.title
+    
+    
+    
