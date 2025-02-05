0d2842bfc73318d71ee65ad6bb911cbb7d634812
Rakibul Hasan <hassanrakibul926@gmail.com>
2025-02-06 02:04:18 +0600

    add budget category

--- utils/views.py
+++ utils/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.models import User
 from django.db import IntegrityError
 from django.db.models import Q
+
+from .models import BudgetCategory, Budget
 # Create your views here.
 
 
@@ -13,8 +15,22 @@ def add_budget_cat(request):
         messages.error(request, f"User  Does not matching.error: {e}")
     if request.method == "POST":
         cat_name = request.POST.get('cat_name')
-        print("''''''''=")
-        print(cat_name)
+        
+        
+        try:
+            add_category = BudgetCategory(
+                user = user,
+                cat_name = cat_name
+            )
+            
+            add_category.save()
+            messages.success(request, "Budget Category successfully added!")
+            return redirect('utils:add_budget_cat')        
+        except IntegrityError:
+             messages.warning(request, 'Error: Unable to added budget category, please try again!')
+             return redirect('utils:add_budget_cat') 
+        
+        
         
     return render (request, 'utils/budget_cat_add.html', context={})
     
