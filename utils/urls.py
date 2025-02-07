345254173c20da4baad0fce8de3364657c67012c
Rakibul Hasan <hassanrakibul926@gmail.com>
2025-02-08 00:03:42 +0600

    create budget and budget category

--- utils/urls.py
+++ utils/urls.py
@@ -4,5 +4,8 @@ from . import views
 app_name='utils'
 
 urlpatterns = [
-    path('add-budget-category/', views.add_budget_cat, name='add_budget_cat')
+    path('add-budget-category/', views.add_budget_cat, name='add_budget_cat'),
+    path('budget-category-list/', views.view_budget_category, name='category_list'),
+    path('add-budget/', views.add_badget, name='add_budget'),
+    path('view-budget-list/', views.view_budget, name='view_budget'),
 ]
--- utils/views.py
+++ utils/views.py
@@ -34,3 +34,66 @@ def add_budget_cat(request):
         
     return render (request, 'utils/budget_cat_add.html', context={})
     
+    
+
+def view_budget_category(request):
+    user = request.user
+    try:
+        budget_categorys = BudgetCategory.objects.filter(user = user)
+    except Exception as e:
+        messages.warning(request, F"Something wrong!Error:{e}")
+        return redirect('utils:add_budget_cat')
+    return render(request, 'utils/budget_cat.html', context={"categorys":budget_categorys})
+
+
+def add_badget(request):
+    user = request.user
+    try:
+        budget_categorys = BudgetCategory.objects.filter(user = user)
+    except Exception as e:
+        messages.warning(request, F"Something wrong!Error:{e}")
+        return redirect('utils:add_budget')
+    
+    if request.method == "POST":
+        budget_title = request.POST.get('budget-title')
+        category_id = request.POST.get('category')
+        budget_amount = request.POST.get('budget-amount')
+        due_date = request.POST.get('budget-due-date')
+        
+        try:
+            category_instance = BudgetCategory.objects.get(id=category_id)
+        except BudgetCategory.DoesNotExist:
+            messages.warning(request, 'Budget not added, something wrong!')
+            return redirect('utils:add_budget')
+        
+        try:
+            budget = Budget(
+                user = user,
+                title=budget_title,
+                category = category_instance,
+                amount = budget_amount,
+                due_date = due_date
+            )
+            
+            budget.save()
+            messages.success(request, "Budget successfully added!")
+            return redirect('utils:add_budget')
+            
+        except IntegrityError:
+            messages.warning(request, 'Budget not added, something wrong!')
+            return redirect('utils:add_budget')
+
+    return render(request, 'utils/add_budget.html', context={'categorys':budget_categorys})
+
+
+def view_budget(request):
+    user = request.user
+    
+    try:
+        budgets = Budget.objects.filter(user=user)
+    except Exception as e:
+        messages.warning(request, 'Budget not added, something wrong!')
+        return redirect('utils:add_budget')
+    
+    return render(request, 'utils/budget_list.html', context={'budgets':budgets})
+    
