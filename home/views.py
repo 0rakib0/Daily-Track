da22ebf352a2b75fe04b537347e3b525dc3d8d9c
Rakibul Hasan <hassanrakibul926@gmail.com>
2025-05-11 00:47:35 +0600

    design deshbord and get 50% data fron DB

--- home/views.py
+++ home/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from transactions.models import TotalBalance, Express, Income
+from django.db.models import Q, Sum
 # Create your views here.
 
 @login_required
 def Dashbord(request):
-    return render(request, 'Home/dashbord.html')
+    total_blance = TotalBalance.objects.get(user=request.user)
+    expenses = Express.objects.filter(Q(user=request.user)).select_related('user')
+    total_expense = expenses.aggregate(Sum('amount'))['amount__sum'] or 0
+    income = Income.objects.filter(Q(user=request.user)).select_related('user')
+    total_income = income.aggregate(Sum('amount'))['amount__sum'] or 0
+    context = {
+        'total_blance':total_blance,
+        'total_expense':total_expense,
+        'total_income':total_income,
+    }
+    
+    return render(request, 'Home/dashbord.html', context)
